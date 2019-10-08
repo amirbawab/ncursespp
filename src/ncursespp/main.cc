7575b76b9c13d658de00e94488de8f76878db6ec
Amir Bawab <amirbawab@gmail.com>
2019-10-08 16:36:31 -0400

    Clean project

--- src/ncursespp/main.cc
+++ src/ncursespp/main.cc
@@ -1,15 +1,6 @@
 #include <iostream>
 #include <ncurses/curses.h>
-#include <ncursespp/window.h>
 
 int main() {
-  npp::WindowOptions terminal_options;
-  terminal_options.show_cursor = false;
-
-  npp::Terminal terminal(terminal_options);
-  terminal.init();
-  terminal.AddChild(&terminal);
-  getch();
-  terminal.end();
   return 0;
 }
